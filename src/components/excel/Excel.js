cdcd426a22804f90f62a201a60d2034341729b54
samolevich <samolevich@gmail.com>
2020-06-14 19:36:40 +0300

    create root tag for all compnents

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,6 +1,11 @@
 export class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector),
-    this.components = options.components || [],
+    this.$el = document.querySelector(selector);
+    this.components = options.components || [];
+  }
+
+  render() {
+    console.log(this.$el);
+    this.$el.insertAdjacentHTML('afterbegin', '<h1>Test</h1>');
   }
 }
